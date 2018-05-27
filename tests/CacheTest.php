78d0b5498154700fb7928b20fd946acf0b4e7444
beMang <antonuttiadrien@gmail.com>
2018-05-27 10:56:50 +0200

    adding setUpBeforeClass

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -7,15 +7,18 @@ use bemang\Cache\FileCache;
 class CacheTest extends \PHPUnit\Framework\TestCase
 {
     const CACHE_PATH = __DIR__ . '/../tmp/';
+    protected static $cacheInstance;
 
-    public function setUp()
+    public static function setUpBeforeClass()
     {
         require_once(__DIR__ . '/../vendor/autoload.php');
+        self::$cacheInstance = new FileCache(self::CACHE_PATH);
     }
+
 
     public function testGetAndSet()
     {
-        $cache = new FileCache(self::CACHE_PATH);
+        $cache = self::$cacheInstance;
         $cache->setMultiple([
         'test1' => 'hello, test1',
         'test2' => 'hello, test2'
